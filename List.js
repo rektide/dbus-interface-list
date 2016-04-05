99112bf5919862bc1aa54dbd005fef5504d07582
rektide <rektide@voodoowarez.com>
2016-04-04 20:28:18 -0400

    Successful recurse into objects.

--- List.js
+++ List.js
@@ -1,40 +1,47 @@
 var
-  introspect= require("./msg/introspect"),
+  introspectMsg= require("./msg/introspect"),
   Messager= require("./Messager"),
   rxjs= require("rxjs"),
   xml2js= require("es6-promisify")(require("xml2js").parseString)
 
 function List( busConnection, destination){
-	var
-	  messager= Messager( busConnection),
-	  subject= rxjs.BehaviorSubject()
-	return rxjs.Observable.create(function( observer){
-		function explore(introspection, path){
+	var messager= Messager( busConnection)
+	return rxjs.Observable.create( function( observer){
+		// recurse into a node, finding all new paths, & issues ask()'s on them
+		function explore( path, introspection){
+
 		}
+		// ask a node for it's introspection, & if it has more nodes under it, ask/explore those too.
 		function ask( path){
 			var
-			  msg= introspect( destination, path),
-			  reply= messager(msg),
-			  process= reply.then(function( m){
+			  q= introspectMsg( destination, path),
+			  a= messager( q),
+			  processed= a.then(function( replyMessage){
 				var
-				  str= m.body[0],
-				  introspection= xml2js(str),
-				  named= introspection.then(function(introspect){
-					// fire off followups
-					var done= explore(path, introspect)
-
+				  replyText= replyMessage.body[ 0],
+				  replyJs= xml2js( replyText),
+				  replyExplored= replyJs.then(function( introspection){
 					// publish result
-					var result= [path, introspect]
-					observer.next(result)
+					observer.next([ path, introspection.node])
 
-					// return when followups finish
-					return done && Promise.all(done) && true
+					// explore subnodes
+					var
+					  subnodes= introspection.node.node,
+					  recursiveAsks= subnodes&& subnodes.map(node=> {
+						var component= node.$.name
+						if( !component){
+							throw new Error("Expected a node name")
+						}
+						return ask( path+ (path=== "/"? "": "/")+ component)
+					  })
+					return recursiveAsks&& Promise.all(recursiveAsks)
 				  })
+				return replyExplored
 			})
-			return process
+			return processed
 		}
 		var root= ask( "/")
-		root.catch(observer.error)
+		root.then(observer.complete.bind(observer), observer.error.bind(observer))
 	})
 }
 
