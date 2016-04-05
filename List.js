227677ea51a5d5776b0fec525b2c9e2efbdd1112
rektide <rektide@voodoowarez.com>
2016-04-04 20:53:53 -0400

    Promises impl.

--- List.js
+++ List.js
@@ -1,48 +1,39 @@
 var
+  aia= require("all-in-all"),
   introspectMsg= require("./msg/introspect"),
   Messager= require("./Messager"),
-  rxjs= require("rxjs"),
   xml2js= require("es6-promisify")(require("xml2js").parseString)
 
+var _resolved= Promise.resolve()
+
 function List( busConnection, destination){
-	var messager= Messager( busConnection)
-	return rxjs.Observable.create( function( observer){
-		// recurse into a node, finding all new paths, & issues ask()'s on them
-		function explore( path, introspection){
-
-		}
+	var
+	  messager= Messager( busConnection),
+	  res= []
+	function ask( path){
 		// ask a node for it's introspection, & if it has more nodes under it, ask/explore those too.
-		function ask( path){
+		var
+		  q= introspectMsg( destination, path),
+		  a= messager( q),
+		  processed= a.then(function( replyMessage){
 			var
-			  q= introspectMsg( destination, path),
-			  a= messager( q),
-			  processed= a.then(function( replyMessage){
+			  replyText= replyMessage.body[ 0],
+			  replyJs= xml2js( replyText),
+			  replyExplored= replyJs.then(function( introspection){
+				// explore subnodes
 				var
-				  replyText= replyMessage.body[ 0],
-				  replyJs= xml2js( replyText),
-				  replyExplored= replyJs.then(function( introspection){
-					// publish result
-					observer.next([ path, introspection.node])
-
-					// explore subnodes
-					var
-					  subnodes= introspection.node.node,
-					  recursiveAsks= subnodes&& subnodes.map(node=> {
-						var component= node.$.name
-						if( !component){
-							throw new Error("Expected a node name")
-						}
-						return ask( path+ (path=== "/"? "": "/")+ component)
-					  })
-					return recursiveAsks&& Promise.all(recursiveAsks)
-				  })
-				return replyExplored
-			})
-			return processed
-		}
-		var root= ask( "/")
-		root.then(observer.complete.bind(observer), observer.error.bind(observer))
-	})
+				  subnodes= introspection.node.node,
+				  recursiveAsks= subnodes&& subnodes.map(node=> ask( path+ (path=== "/"? "": "/")+ node.$.name)),
+				  solution= recursiveAsks&& Promise.all( recursiveAsks)||  _resolved
+				return solution.then(function(){ return [path, introspection.node]})
+			  })
+			return replyExplored
+		  })
+		res.push(processed)
+		return processed
+	}
+	ask("/")
+	return aia(res)
 }
 
 module.exports= List
